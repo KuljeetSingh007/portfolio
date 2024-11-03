57ad66083a0431504bc6153ec5ad4e4e58757e4d
Kuljeet <iamkuljeet007@gmail.com>
2024-11-03 20:38:21 +0530

    styles improved
    responsive contact section.

--- src/tabs/summary.js
+++ src/tabs/summary.js
@@ -7,11 +7,7 @@ export default function Summary() {
         <div className="summary">
             <LoadingBarComponent color={"#EEEEEE"} />
             <h2>Summary</h2>
-
             <p>Aspiring full stack developer with a solid foundation in HTML, CSS, JavaScript, React.js, and Node.js. Eager to leverage skills in both front-end and back-end development to create robust, user-friendly applications. Committed to continuous learning and adapting to new technologies, seeking an opportunity to contribute to innovative projects and grow within a collaborative team environment.</p>
-
-
-
         </div>
     )
 }

fe2013af0fe373666ff0aa8d76e3b1ed2ffe57cf
Kuljeet <iamkuljeet007@gmail.com>
2025-06-06 15:37:50 +0530

    contact updated
    Now contact form is working. You will recieve email if somebody submit the form. congratulations!

--- src/tabs/contact.js
+++ src/tabs/contact.js
@@ -1,23 +1,53 @@
 import React from 'react'
 import useLoadingNavigation from '../components/navigationFunc';
+import Swal from 'sweetalert2';
 
 export default function Contact() {
     const { LoadingBarComponent } = useLoadingNavigation();
+
+    // web3 forms react setup..
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "cd39e2b2-478f-4a14-9f0d-3d25758b10c7");
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: json
+        }).then((res) => res.json());
+
+        if (res.success) {
+            Swal.fire({
+                title: "Success!",
+                text: "Email sent succesfully!",
+                icon: "success"
+              });
+        }
+    };
+    // web3forms setup end here..
 
     return (
         <div className="contact">
             <LoadingBarComponent color={"#EEEEEE"} />
             <div className="contact-item">
-                <form>
+                <form onSubmit={onSubmit}>
                     <div class="form-row">
-                        <input type="text" placeholder="Full Name" required />
-                        <input type="text" placeholder="Email" required />
+                        <input name='name' type="text" placeholder="Full Name" required />
+                        <input name='email' type="text" placeholder="Email" required />
                     </div>
                     <div class="form-row">
-                        <input type="tel" placeholder="Phone Number" required />
+                        <input name='phone' type="tel" placeholder="Phone Number" required />
                     </div>
                     <div class="form-row">
-                        <textarea placeholder="Leave a message..." required></textarea>
+                        <textarea name='message' placeholder="Leave a message..." required></textarea>
                     </div>
                     <div class="form-row">
                         <button type="submit">Contact Me</button>
